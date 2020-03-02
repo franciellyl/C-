0392e7593ccc4bb11140d8246ec52eb0c9f107ed
Francielly Lima <45133442+franciellyl@users.noreply.github.com>
2020-03-02 11:08:39 -0300

    Create TrabalhandoComListas_1.cs

---
+++ TrabalhandoComListas_1.cs
@@ -0,0 +1,42 @@
+/*
+Project: TrabalhandoComListas
+Author: Francielly Lima
+Date: 02/03/2020
+Description: Classe que realiza a soma dos números de 1 a 20 que são dividíveis por 3.
+*/
+
+using System;
+
+namespace TrabalhandoComListas
+{
+    public class Listas
+    {
+
+      var names = new List<string>{"Fran", "Ana", "Edu"};
+      foreach (var name in names){
+       Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+
+//----------------------------
+//Adicionar ou Remover um nome
+//----------------------------
+      Console.WriteLine(); //Imprime uma linha em branco.
+      names.Add("Maria");
+      names.Add("Bill");
+      names.Remove("Ana");
+      
+      foreach(var name in names){
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+
+//----------------------------
+//Por índices
+//----------------------------
+      Console.WriteLine();
+      Console.WriteLine($"My name is {names[0]}.");
+      Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
+
+      Console.WriteLine($"The list has {names.Count} people in it"); //Contabilida a quantidade de itens na lista.
+      
+   }
+}
